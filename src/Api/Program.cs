8deb65b67e9405d302ce7e4fdcd426f11206ab16
Фаридун Бердиев <madysoftware@gmail.com>
2022-11-21 22:01:26 +0500

    created service registrations

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,26 +1,11 @@
-using Entity.Configs;
-using Microsoft.Extensions.Options;
-using UseCase.Abstraction.FibonacciSubsequence;
-using UseCase.Implementation.FibonacciSubsequence;
-using Repository.Implementation.FibonacciSubsequence;
-using Repository.Abstraction.FibonacciSubsequence;
+using Presentation.IoC;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// TODO: Create file that stores all services configs.
-builder.Services.AddTransient<IFibonacciSubsequenceFromCacheUseCase, FibonacciSubsequenceFromCacheInteractor>();
-builder.Services.AddTransient<IFibonacciSubsequenceGeneratorUseCase, FibonacciSubsequenceGeneratorInteractor>();
-builder.Services.AddTransient<IFibonacciSubsequenceProviderUseCase, FibonacciSubsequenceProviderInteractor>();
-builder.Services.AddTransient<IFibonacciSubsequenceRepository, FibonacciSubsequenceRepository>();
-
-builder.Services.Configure<DistributedCacheOption>(builder.Configuration.GetSection("DistributedCache"));
-builder.Services.AddSingleton(resolver => resolver.GetRequiredService<IOptions<DistributedCacheOption>>().Value);
-
-builder.Services.AddStackExchangeRedisCache(options =>
- {
-     options.Configuration = "localhost:6379";
-     options.InstanceName = "SampleInstance";
- });
+builder.RegisterConfigs();
+builder.RegisterService();
+builder.RegisterDistributedCache();
+builder.RegisterValidators();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
